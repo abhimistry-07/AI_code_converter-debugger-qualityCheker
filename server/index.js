335294121753895154424002d47a568d65b1a30a
Abhishek <mistryabhishek2000@gmail.com>
2023-09-14 08:29:09 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const { ConversionRouter } = require('./router/conversionRoute');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const apiKey = process.env.OPENAI_API_KEY;
-const OpenAI = require('openai');
+// const OpenAI = require('openai');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.use(express.json());
 app.use(cors());
@@ -16,164 +17,164 @@ app.get('/', (req, res) => {
 });
 
 // app.use('/convert', ConversionRouter);
-const openai = new OpenAI({
-    apiKey
-});
+// const openai = new OpenAI({
+//     apiKey
+// });
 
-async function generateCode(prompt) {
-    try {
-        const completion = await openai.chat.completions.create({
-            messages: [{ role: 'user', content: prompt }],
-            model: 'gpt-3.5-turbo',
-        });
-        // console.log(completion.choices[0])
-        return completion.choices[0].message.content
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-app.post('/convert', async (req, res) => {
-    try {
-        const { code, language } = req.body;
-
-        const response = await generateCode(`Convert the given below code to ${language} \n code ${code}`);
-
-        res.status(200).send({ response });
-    } catch (error) {
-        console.error('Error converting code:', error);
-        res.status(400).json({ error: 'Internal server error' });
-    }
-});
-
-app.post("/debug", async (req, res) => {
-    try {
-        const { code } = req.body;
-        const response = await generateCode(`Debug the given below code with explaination : \n ${code}`);
-        res.status(200).json({ response })
-    } catch (error) {
-        console.error("Error", error);
-        res.status(400).json({ error: 'Internal server error' });
-    }
-});
-
-app.post('/quality', async (req, res) => {
-    try {
-        const { code } = req.body;
-
-        const response = await generateCode(`I want you to act as a debuggr tool. Debug the following code:${code} and generate response. The response should contain errors if there any, provide suggestions for best code.`)
-
-        const formattedDebugedCode = response.match(/.{1,50}/g).join('\n');
-
-        res.status(200).send({ response: formattedDebugedCode });
-    } catch (error) {
-        console.error('Error converting code:', error);
-        res.status(400).json({ error: 'Internal server error' });
-    }
-});
-
-// -----------------------
+// async function generateCode(prompt) {
+//     try {
+//         const completion = await openai.chat.completions.create({
+//             messages: [{ role: 'user', content: prompt }],
+//             model: 'gpt-3.5-turbo',
+//         });
+//         // console.log(completion.choices[0])
+//         return completion.choices[0].message.content
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 // app.post('/convert', async (req, res) => {
 //     try {
 //         const { code, language } = req.body;
 
-//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-//             method: "POST",
-//             headers: {
-//                 "Authorization": `Bearer ${apiKey}`,
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 model: "gpt-3.5-turbo",
-//                 messages: [{ role: "user", content: `convert ${code} into ${language} code just give the code only` }],
-//                 max_tokens: 1000
-//             })
-//         });
+//         const response = await generateCode(`Convert the given below code to ${language} \n code ${code}`);
 
-//         response = await response.json();
-
-//         // Check if response.choices is defined and not empty
-//         if (response.choices && response.choices.length > 0) {
-//             const data = response.choices[0].message.content;
-
-//             res.status(200).json({ "code": data });
-//         } else {
-//             // Handle the case when response.choices is empty
-//             res.status(500).send({ msg: "No valid response from the API" });
-//         }
+//         res.status(200).send({ response });
 //     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ msg: error.message });
+//         console.error('Error converting code:', error);
+//         res.status(400).json({ error: 'Internal server error' });
 //     }
-// })
+// });
 
-// app.post('/debug', async (req, res) => {
+// app.post("/debug", async (req, res) => {
 //     try {
 //         const { code } = req.body;
-
-//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-//             method: "POST",
-//             headers: {
-//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 model: "gpt-3.5-turbo",
-//                 messages: [{ role: "user", content: `Debug this  ${code} and provide correct output` }],
-//                 max_tokens: 1000
-//             })
-//         });
-
-//         response = await response.json();
-
-//         // Check if response.choices is defined and not empty
-//         if (response.choices && response.choices.length > 0) {
-//             const data = response.choices[0].message.content;
-//             res.status(200).send({ debugData: data });
-//         } else {
-//             // Handle the case when response.choices is empty
-//             res.status(500).send({ "msg": "No valid response from the API" });
-//         }
+//         const response = await generateCode(`Debug the given below code with explaination : \n ${code}`);
+//         res.status(200).json({ response })
 //     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ msg: error.message });
+//         console.error("Error", error);
+//         res.status(400).json({ error: 'Internal server error' });
 //     }
-// })
+// });
 
 // app.post('/quality', async (req, res) => {
 //     try {
 //         const { code } = req.body;
 
-//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-//             method: "POST",
-//             headers: {
-//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({
-//                 model: "gpt-3.5-turbo",
-//                 messages: [{ role: "user", content: `check this provided  ${code} Quality and give feedback acording to that in 100 words` }],
-//                 max_tokens: 1000
-//             })
-//         });
+//         const response = await generateCode(`I want you to act as a debuggr tool. Debug the following code:${code} and generate response. The response should contain errors if there any, provide suggestions for best code.`)
 
+//         const formattedDebugedCode = response.match(/.{1,50}/g).join('\n');
 
-//         response = await response.json();
-
-//         // Check if response.choices is defined and not empty
-//         if (response.choices && response.choices.length > 0) {
-//             const data = response.choices[0].message.content;
-//             res.status(200).send({ codeQuality: data });
-//         } else {
-//             // Handle the case when response.choices is empty
-//             res.status(500).send({ msg: "No valid response from the API" });
-//         }
+//         res.status(200).send({ response: formattedDebugedCode });
 //     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ msg: error.message });
+//         console.error('Error converting code:', error);
+//         res.status(400).json({ error: 'Internal server error' });
 //     }
-// })
+// });
+
+// -----------------------
+
+app.post('/convert', async (req, res) => {
+    try {
+        const { code, language } = req.body;
+
+        let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${apiKey}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: `convert ${code} into ${language} code just give the code only` }],
+                max_tokens: 1000
+            })
+        });
+
+        response = await response.json();
+
+        // Check if response.choices is defined and not empty
+        if (response.choices && response.choices.length > 0) {
+            const data = response.choices[0].message.content;
+
+            res.status(200).json({ code: data });
+        } else {
+            // Handle the case when response.choices is empty
+            res.status(500).send({ msg: "No valid response from the API" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ msg: error.message });
+    }
+})
+
+app.post('/debug', async (req, res) => {
+    try {
+        const { code } = req.body;
+
+        let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: `Debug this  ${code} and provide correct output` }],
+                max_tokens: 1000
+            })
+        });
+
+        response = await response.json();
+
+        // Check if response.choices is defined and not empty
+        if (response.choices && response.choices.length > 0) {
+            const data = response.choices[0].message.content;
+            res.status(200).send({ debugData: data });
+        } else {
+            // Handle the case when response.choices is empty
+            res.status(500).send({ "msg": "No valid response from the API" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ msg: error.message });
+    }
+})
+
+app.post('/quality', async (req, res) => {
+    try {
+        const { code } = req.body;
+
+        let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: `check this provided  ${code} Quality and give feedback acording to that in 100 words` }],
+                max_tokens: 1000
+            })
+        });
+
+
+        response = await response.json();
+
+        // Check if response.choices is defined and not empty
+        if (response.choices && response.choices.length > 0) {
+            const data = response.choices[0].message.content;
+            res.status(200).send({ codeQuality: data });
+        } else {
+            // Handle the case when response.choices is empty
+            res.status(500).send({ msg: "No valid response from the API" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ msg: error.message });
+    }
+})
 
 // ------------------------
 
