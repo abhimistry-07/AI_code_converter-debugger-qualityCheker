c5b18bf2a3979c8146e6be57d1b9b91b83f4cc4c
Abhishek <mistryabhishek2000@gmail.com>
2023-09-14 08:12:30 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -33,7 +33,6 @@ async function generateCode(prompt) {
     }
 }
 
-
 app.post('/convert', async (req, res) => {
     try {
         const { code, language } = req.body;
@@ -72,6 +71,111 @@ app.post('/quality', async (req, res) => {
         res.status(400).json({ error: 'Internal server error' });
     }
 });
+
+// -----------------------
+
+// app.post('/convert', async (req, res) => {
+//     try {
+//         const { code, language } = req.body;
+
+//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${apiKey}`,
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `convert ${code} into ${language} code just give the code only` }],
+//                 max_tokens: 1000
+//             })
+//         });
+
+//         response = await response.json();
+
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
+
+//             res.status(200).json({ "code": data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ msg: "No valid response from the API" });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
+//     }
+// })
+
+// app.post('/debug', async (req, res) => {
+//     try {
+//         const { code } = req.body;
+
+//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `Debug this  ${code} and provide correct output` }],
+//                 max_tokens: 1000
+//             })
+//         });
+
+//         response = await response.json();
+
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
+//             res.status(200).send({ debugData: data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ "msg": "No valid response from the API" });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
+//     }
+// })
+
+// app.post('/quality', async (req, res) => {
+//     try {
+//         const { code } = req.body;
+
+//         let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `check this provided  ${code} Quality and give feedback acording to that in 100 words` }],
+//                 max_tokens: 1000
+//             })
+//         });
+
+
+//         response = await response.json();
+
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
+//             res.status(200).send({ codeQuality: data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ msg: "No valid response from the API" });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
+//     }
+// })
+
+// ------------------------
 
 // app.post('/convert', async (req, res) => {
 //     try {
