6a3442a3956053652f867279fb5d27388494e1cc
Abhishek <mistryabhishek2000@gmail.com>
2023-09-14 08:51:11 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -75,203 +75,203 @@ app.get('/', (req, res) => {
 
 // -----------------------
 
-app.post('/convert', async (req, res) => {
-    try {
-        const { code, language } = req.body;
-
-        let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${apiKey}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: `convert ${code} into ${language} code just give the code only` }],
-                max_tokens: 1000
-            })
-        });
-
-        response = await response.json();
-
-        // Check if response.choices is defined and not empty
-        if (response.choices && response.choices.length > 0) {
-            const data = response.choices[0].message.content;
-
-            res.status(200).json({ code: data });
-        } else {
-            // Handle the case when response.choices is empty
-            res.status(500).send({ msg: "No valid response from the API" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ msg: error.message });
-    }
-})
-
-app.post('/debug', async (req, res) => {
-    try {
-        const { code } = req.body;
-
-        let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: `Debug this  ${code} and provide correct output` }],
-                max_tokens: 1000
-            })
-        });
-
-        response = await response.json();
-
-        // Check if response.choices is defined and not empty
-        if (response.choices && response.choices.length > 0) {
-            const data = response.choices[0].message.content;
-            res.status(200).send({ debugData: data });
-        } else {
-            // Handle the case when response.choices is empty
-            res.status(500).send({ "msg": "No valid response from the API" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ msg: error.message });
-    }
-})
-
-app.post('/quality', async (req, res) => {
-    try {
-        const { code } = req.body;
-
-        let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: `check this provided  ${code} Quality and give feedback acording to that in 100 words` }],
-                max_tokens: 1000
-            })
-        });
-
-
-        response = await response.json();
-
-        // Check if response.choices is defined and not empty
-        if (response.choices && response.choices.length > 0) {
-            const data = response.choices[0].message.content;
-            res.status(200).send({ codeQuality: data });
-        } else {
-            // Handle the case when response.choices is empty
-            res.status(500).send({ msg: "No valid response from the API" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ msg: error.message });
-    }
-})
-
-// ------------------------
-
 // app.post('/convert', async (req, res) => {
 //     try {
 //         const { code, language } = req.body;
 
-//         const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+//         let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${apiKey}`,
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `convert ${code} into ${language} code just give the code only` }],
+//                 max_tokens: 1000
+//             })
+//         });
 
-//         const headers = {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${apiKey}`,
-//         };
+//         response = await response.json();
 
-//         const requestData = {
-//             prompt: `Convert the following ${language} code:\n\n${code}\n\nTo:\n\n`,
-//             max_tokens: 100,
-//         };
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
 
-//         const response = await axios.post(apiUrl, requestData, { headers });
-
-//         const convertedCode = response.data.choices[0].text;
-
-//         res.status(200).send({ convertedCode });
+//             res.status(200).json({ code: data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ msg: "No valid response from the API" });
+//         }
 //     } catch (error) {
-//         console.error('Error converting code:', error);
-//         res.status(400).json({ error: 'Internal server error' });
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
 //     }
-// });
+// })
 
 // app.post('/debug', async (req, res) => {
 //     try {
 //         const { code } = req.body;
 
-//         // console.log(code);
+//         let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `Debug this  ${code} and provide correct output` }],
+//                 max_tokens: 1000
+//             })
+//         });
 
-//         const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+//         response = await response.json();
 
-//         const headers = {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${apiKey}`,
-//         };
-
-//         const prompt = `I want you to act as a debuggr tool. Debug the following code:${code} and generate response. The response should contain errors if there any, provide suggestions for best code.`
-
-//         const requestData = {
-//             prompt,
-//             max_tokens: 100,
-//             temperature: 0.7,
-//         };
-
-//         const response = await axios.post(apiUrl, requestData, { headers });
-//         const debugedCode = response.data.choices[0].text;
-//         const formattedDebugedCode = debugedCode.match(/.{1,50}/g).join('\n');
-
-//         // console.log(debugedCode);
-
-//         res.send({ debugedCode: formattedDebugedCode });
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
+//             res.status(200).send({ debugData: data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ "msg": "No valid response from the API" });
+//         }
 //     } catch (error) {
-//         console.error('Error converting code:', error);
-//         res.status(500).json({ error: 'Internal server error' });
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
 //     }
-// });
+// })
 
 // app.post('/quality', async (req, res) => {
 //     try {
 //         const { code } = req.body;
 
-//         // console.log(code);
+//         let response = await fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
+//             method: "POST",
+//             headers: {
+//                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//                 model: "gpt-3.5-turbo",
+//                 messages: [{ role: "user", content: `check this provided  ${code} Quality and give feedback acording to that in 100 words` }],
+//                 max_tokens: 1000
+//             })
+//         });
 
-//         const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
 
-//         const headers = {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${apiKey}`,
-//         };
+//         response = await response.json();
 
-//         const prompt = `Check th equality of code ${code}`
-
-//         const requestData = {
-//             prompt,
-//             max_tokens: 100,
-//             temperature: 0.7,
-//         };
-
-//         const response = await axios.post(apiUrl, requestData, { headers });
-//         const debugedCode = response.data.choices[0].text;
-//         const formattedDebugedCode = debugedCode.match(/.{1,50}/g).join('\n');
-
-//         // console.log(debugedCode);
-
-//         res.send({ debugedCode: formattedDebugedCode });
+//         // Check if response.choices is defined and not empty
+//         if (response.choices && response.choices.length > 0) {
+//             const data = response.choices[0].message.content;
+//             res.status(200).send({ codeQuality: data });
+//         } else {
+//             // Handle the case when response.choices is empty
+//             res.status(500).send({ msg: "No valid response from the API" });
+//         }
 //     } catch (error) {
-//         console.error('Error converting code:', error);
-//         res.status(500).json({ error: 'Internal server error' });
+//         console.log(error);
+//         res.status(500).send({ msg: error.message });
 //     }
-// });
+// })
+
+// ------------------------
+
+app.post('/convert', async (req, res) => {
+    try {
+        const { code, language } = req.body;
+
+        const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
+        };
+
+        const requestData = {
+            prompt: `Convert the following ${language} code:\n\n${code}\n\nTo:\n\n`,
+            max_tokens: 1000,
+        };
+
+        const response = await axios.post(apiUrl, requestData, { headers });
+
+        const convertedCode = response.data.choices[0].text;
+
+        res.status(200).send({ convertedCode });
+    } catch (error) {
+        console.error('Error converting code:', error);
+        res.status(400).json({ error: 'Internal server error' });
+    }
+});
+
+app.post('/debug', async (req, res) => {
+    try {
+        const { code } = req.body;
+
+        // console.log(code);
+
+        const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
+        };
+
+        const prompt = `I want you to act as a debuggr tool. Debug the following code:${code} and generate response. The response should contain errors if there any, provide suggestions for best code.`
+
+        const requestData = {
+            prompt,
+            max_tokens: 1000,
+            temperature: 0.7,
+        };
+
+        const response = await axios.post(apiUrl, requestData, { headers });
+        const debugedCode = response.data.choices[0].text;
+        const formattedDebugedCode = debugedCode.match(/.{1,50}/g).join('\n');
+
+        // console.log(debugedCode);
+
+        res.send({ debugedCode: formattedDebugedCode });
+    } catch (error) {
+        console.error('Error converting code:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.post('/quality', async (req, res) => {
+    try {
+        const { code } = req.body;
+
+        // console.log(code);
+
+        const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`,
+        };
+
+        // const prompt = `Check the quality of this code ${code}`
+
+        const requestData = {
+            prompt: `Do the deep quality assesment and give a brief description about the code ${code} and check if it adhere to the standard coding practices or not, and also give a brief about the concepts used in the code`,
+            max_tokens: 1000,
+            temperature: 0.7,
+        };
+
+        const response = await axios.post(apiUrl, requestData, { headers });
+        const debugedCode = response.data.choices[0].text;
+        const formattedDebugedCode = debugedCode.match(/.{1,50}/g).join('\n');
+
+        // console.log(debugedCode);
+
+        res.send({ codeQuality: formattedDebugedCode });
+    } catch (error) {
+        console.error('Error converting code:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.listen(PORT, () => {
     try {
